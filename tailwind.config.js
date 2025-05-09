40e961d9411ebae44ffb81ba64427f547d461043
tammo-v <tammo@symboliccomputing.com>
2025-05-09 13:23:38 +0200

    Extend Tailwind Typography to remove background from code and pre elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     './src/**/*.{astro,js,ts,jsx,tsx,md}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            pre: {
+              backgroundColor: 'transparent',
+              padding: '0',
+              borderRadius: '0',
+              boxShadow: 'none',
+            },
+            code: {
+              backgroundColor: 'transparent',
+              padding: '0',
+              borderRadius: '0',
+              boxShadow: 'none',
+              color: '#1f2937', // Tailwind gray-800
+            },
+            'code::before': { content: 'none' },
+            'code::after': { content: 'none' },
+          },
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
-}
+};
